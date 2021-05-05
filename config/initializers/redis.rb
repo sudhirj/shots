8c57ef92fb1a65db5876b3f6c7ba7bb92e46d1f8
Sudhir Jonathan <sudhir.j@gmail.com>
2021-05-06 01:19:10 +0530

    Corrected redis URL.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 pool_size = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-$redis = ConnectionPool.new(size: pool_size, timeout: 5) { Redis.new(url: ENV.fetch('REDIS_URL')) }
+$redis = ConnectionPool.new(size: pool_size, timeout: 5) { Redis.new(url: ENV.fetch('REDISCLOUD_URL')) }
 $redirect = ConnectionPool::Wrapper.new(size: 3, timeout: 3) { Redis.new }
