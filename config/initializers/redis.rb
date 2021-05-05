fee276a17a3f418beff09a971baa797101637d6a
Sudhir Jonathan <sudhir.j@gmail.com>
2021-05-06 01:12:56 +0530

    Prep for Heroku.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,5 @@
-$redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new }
+# frozen_string_literal: true
+
+pool_size = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+$redis = ConnectionPool.new(size: pool_size, timeout: 5) { Redis.new(url: ENV.fetch('REDIS_URL')) }
 $redirect = ConnectionPool::Wrapper.new(size: 3, timeout: 3) { Redis.new }
