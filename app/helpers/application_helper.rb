3dc7555945d8f5f03bcec4ef005feb5ab18ef47c
Sudhir Jonathan <sudhir.j@gmail.com>
2021-05-12 01:36:54 +0530

    Added canonical link.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def canonicalize(url)
+    uri = URI.parse url
+    uri.host = 'taketheshot.in'
+    uri.scheme = 'https'
+    uri.port = nil
+    uri.to_s.html_safe
+  end
 end
