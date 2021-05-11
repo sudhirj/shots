c0493f1b7ddf93bfd34d2ab4128fa833713d971f
Sudhir Jonathan <sudhir.j@gmail.com>
2021-05-12 00:48:47 +0530

    Added new domain.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ Rails.application.configure do
 
   config.hosts << 'shots.sudhir.io'
   config.hosts << 'shots-sudhir-io.herokuapp.com'
+  config.hosts << 'taketheshot.in'
 
   config.lograge.enabled = true
   # Include generic and useful information about system operation, but avoid logging too much
