885df81210b2d972a220f73207257b7e02baaeec
Sudhir Jonathan <sudhir.j@gmail.com>
2021-05-22 19:19:07 +0530

    Added sitemap

---
+++ config/sitemap.rb
@@ -0,0 +1,30 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://taketheshot.in"
+
+SitemapGenerator::Sitemap.create do
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+  Pincode.find_each do |pincode|
+    add pincode_path(pincode.id), changefreq: 'daily'
+  end
+end
